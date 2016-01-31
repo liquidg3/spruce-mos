1a4ade469e0091ec166e0f2983a579f4292044d2
Tay <me@taylorrome.ro>
2016-01-31 15:52:24 -0700

    setup changes

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 
 files = ["mos/*"]
 
@@ -7,10 +7,14 @@ setup(name="mos",
       author="Taylor Romero",
       author_email="taylor@spruce.me",
       packages=['mos'],
+      dependency_links=[
+          'https://github.com/adafruit/Adafruit_Python_GPIO.git@master#egg=Adafruit-GPIO',
+          'https://github.com/adafruit/Adafruit_Python_LED_Backpack.git@master#egg=Adafruit-LED-Backpack'
+      ],
       install_requires=[
           'SocketIO-client',
           'argparse',
-          'git+https://github.com/adafruit/Adafruit_Python_GPIO.git@master',
-          'git+https://github.com/adafruit/Adafruit_Python_LED_Backpack.git@master'
+          'Adafruit-GPIO',
+          'Adafruit-LED-Backpack'
       ],
       scripts=['bin/mos'])
